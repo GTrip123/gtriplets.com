6d63fd5fb3fbcae2cd36cafa3f3d720d72e44c27
gtripletsyt <108158973+gtripletsyt@users.noreply.github.com>
2026-07-26 19:07:18 -0500

    test
    hopefully fix??

--- functions/api/postUpdate.js
+++ functions/api/postUpdate.js
@@ -1,37 +1,20 @@
 export async function onRequestPost(context) {
-  const SECRET = "c3d92b0d-7fe2-4b9b-8a31-your-secret-here";
+  const updates = JSON.parse(
+    (await context.env.UPDATES.get("updates")) || "[]"
+  );
 
-  try {
-    const body = await context.request.json();
+  updates.unshift({
+    title: "test",
+    description: "beta test fixing",
+    author: "gtripletsyt",
+    date: new Date().toISOString(),
+    url: "#"
+  });
 
-    if (body.secret !== SECRET) {
-      return new Response("Unauthorized", { status: 401 });
-    }
+  await context.env.UPDATES.put(
+    "updates",
+    JSON.stringify(updates)
+  );
 
-    const updates = JSON.parse(
-      (await context.env.UPDATES.get("updates")) || "[]"
-    );
-
-    updates.unshift({
-      title: body.title,
-      description: body.description,
-      author: body.author,
-      date: new Date().toISOString(),
-      url: body.url || "#"
-    });
-
-    await context.env.UPDATES.put(
-      "updates",
-      JSON.stringify(updates)
-    );
-
-    return Response.json({
-      success: true
-    });
-
-  } catch (err) {
-    return new Response(err.toString(), {
-      status: 500
-    });
-  }
+  return Response.json({ success: true });
 }
